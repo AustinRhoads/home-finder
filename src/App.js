0524379bdab2993b9c4f8c31e51eb674812f320e
Austin Rhoads <austinrrhoads@ymail.com>
2021-12-11 17:53:06 -0600

    debugged npm start and build with openssl

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect} from 'react'
 
 function App() {
+
+  
+  
+
+  useEffect(() => {
+   // const { ZSID } = process.env;
+    console.log(process.env.ZSID)
+  })
   return (
+   
     <div className="App">
         <h1>Home Finder App</h1>
+      
+
     </div>
   );
 }
